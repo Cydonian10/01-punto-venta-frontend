f08b218cb1758462f6c5b7e6d45a1133984defcd
Gabriel <cydonian_07@hotmail.com>
2023-05-28 21:48:11 -0500

    talwind y material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, effect, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent 
+{
+  public title = signal("")
+
+  public cambiar = effect(() => {
+    console.log(this.title());
+  })
+ 
+  public changeTitle () : void {
+    this.title.update(() => "gabriel");
+  }
 }
